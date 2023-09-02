d6b3a4088481ea3e15f0c66ef74664b893d82ce0
pavan-bandi <vigneshreddy94918@gmail.com>
2023-09-02 12:45:27 +0530

    2 line

--- BackEnd/one.js
+++ BackEnd/one.js
@@ -1,2 +1,14 @@
-console.log("Hello")
-console.log("dwjdiwehdj")
+
+function factorialRecursive(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorialRecursive(n - 1);
+    }
+  }
+  
+  // Example usage:
+  const num = 5;
+  const result = factorialRecursive(num);
+  console.log(`The factorial of ${num} is ${result}`);
+  
